8ed69f9136bbba76fe858e858d3aa13c83c11f57
Kale-Eos <pacoellaga@gmail.com>
2019-06-02 23:26:20 -0700

    created drag and drop system, need to set colliders and parameters

---
+++ ExtraCreditGamJam4_KaleEos/Assets/Scripts/UI/ObjectDrop.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Spawn : MonoBehaviour
+{
+    public GameObject item;
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public void SpawnDroppedItem()
+    {
+        // GameObject itemInstance;
+
+        Vector2 playerPos = new Vector2(player.position.x, player.position.y - 2);
+        // itemInstance = Instantiate(item, playerPos, Quaternion.identity) as GameObject;
+        // itemInstance.GetComponent<Rigidbody2D>().AddForce(itemInstance.transform.up * 0);
+    }
+}
+
+public class ObjectDrop : MonoBehaviour
+{
+    private Inventory inventory;
+    public int i;
+
+    public GameObject item;
+    private Transform player;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    private void Update()
+    {
+        if (transform.childCount <= 0)
+        {
+            inventory.isFull[i] = false;
+        }
+    }
+
+    public void DropItem()
+    {
+        foreach (Transform child in transform)
+        {
+            child.GetComponent<Spawn>().SpawnDroppedItem();
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+}
---
+++ ExtraCreditGamJam4_KaleEos/Assets/Scripts/UI/ObjectPlace.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPlace : MonoBehaviour
+{
+    public GameObject effect;
+    private Transform player;
+
+    private bool selected;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        if (selected == true)
+        {
+            Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector2(cursorPos.x, cursorPos.y);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            selected = false;
+        }
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            selected = true;
+        }
+    }
+}
